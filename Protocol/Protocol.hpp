8cf680449199d83a3028f8163c3dec9a43d70c3b
Paul Crz <paul.creze@epitech.eu>
2021-05-09 20:31:25 +0200

    fix

--- Protocol/Protocol.hpp
+++ Protocol/Protocol.hpp
@@ -24,7 +24,7 @@ namespace Protocol
     struct BoardSize
     {
         std::uint16_t width { 0u };
-        std::uint16_t height { 0u };
+        std::uint16_t heigth { 0u };
     };
 
     /** @brief Hardware connection type */
